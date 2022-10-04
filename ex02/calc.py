cb5c994260f5d3f3516ac4f5791c0585b49230ce
c0b2116145 <c0b2116145@edu.teu.ac.jp>
2022-10-04 16:54:04 +0900

    追加機能

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import math
 def button_ref(event):  #練習3
     btn = event.widget
     txt = btn["text"]
@@ -11,10 +12,32 @@ def button_eq(a):
     entry.delete(0, tk.END)
     entry.insert(tk.END, num)
 
+def button_del(d):
+    entry.delete(0, tk.END)
+
+def button_in(i):
+    suu = int(entry.get())
+    factor = {}
+    div = 2
+    s = math.sqrt(suu)
+    while div < s:
+        div_cnt = 0
+        while suu % div == 0:
+            div_cnt += 1
+            suu //= div
+        if div_cnt != 0:
+            factor[div] = div_cnt
+        div += 1
+    if suu > 1:
+        factor[suu] = 1
+    for s, k in factor.items():
+        num = f"{s}:{k}個"
+        entry.insert(tk.END, num)
+
 root = tk.Tk()
 root.geometry("300x500") #練習1
 for i in range(10): #練習2
-    bt = tk.Button(root, text=f"{9-i}", width=4, height=2, font=("", 30))
+    bt = tk.Button(root, text=f"{9-i}", width=4, height=1, font=("", 30))
     bt.bind("<1>", button_ref)
     if i < 3:
         bt.grid(row=1, column=i)
@@ -24,13 +47,25 @@ for i in range(10): #練習2
         bt.grid(row=3, column=i-6)
     else:
         bt.grid(row=4, column=i-9)
-bt_p = tk.Button(root, text="+", width=4, height=2, font=("", 30)) #練習6
-bt_p.bind("<1>", button_ref)
-bt_p.grid(row=4, column=1)
-
-bt_e = tk.Button(root, text="=", width=4, height=2, font=("", 30)) #練習7
-bt_e.bind("<1>", button_eq)
-bt_e.grid(row=4, column=2)
+n = ["+", "-", "÷", "×", "=", "CA", "因"]
+r = 4
+for i, n in enumerate(n, 1):
+    bt_p = tk.Button(root, text=n, width=4, height=1, font=("", 30)) #練習6
+    i = i % 3
+    if  i==0:
+        r += 1
+    if n=="=":
+        bt_p.bind("<1>", button_eq)
+        bt_p.grid(row=r, column=i)
+    elif n=="CA":
+        bt_p.bind("<1>", button_del)
+        bt_p.grid(row=r, column=i)
+    elif n=="因":
+        bt_p.bind("<1>", button_in)
+        bt_p.grid(row=r, column=i)
+    else:
+        bt_p.bind("<1>", button_ref)
+        bt_p.grid(row=r, column=i)    
 
 entry = tk.Entry(justify="right", width=10, font=("", 40)) #練習4
 entry.grid(row=0, column=0, columnspan=3)
